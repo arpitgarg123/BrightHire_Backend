aec61cd72e0629e2df7c087a79cf6983430f2dad
arpitgarg123 <arpitgarg424@gmail.com>
2024-11-06 15:53:46 +0530

    url changes

--- backend/app.js
+++ backend/app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 
 
 const corsOptions = {  
-    origin: 'http://localhost:5173',
+    origin: 'https://brighthire-backend.onrender.com',
     credentials: true
 }
 
--- frontend/src/utils/Constant.js
+++ frontend/src/utils/Constant.js
@@ -1,4 +1,4 @@
-export const USER_END_POINT_API = 'http://localhost:3000/user';
-export const JOBS_END_POINT_API = 'http://localhost:3000/job';
-export const APPLICANT_END_POINT_API = 'http://localhost:3000/applicant';
-export const COMPANY_END_POINT_API = 'http://localhost:3000/company';
+export const USER_END_POINT_API = 'https://brighthire-backend.onrender.com/user';
+export const JOBS_END_POINT_API = 'https://brighthire-backend.onrender.com/job';
+export const APPLICANT_END_POINT_API = 'https://brighthire-backend.onrender.com/applicant';
+export const COMPANY_END_POINT_API = 'https://brighthire-backend.onrender.com/company';
