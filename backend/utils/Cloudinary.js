59eb01fed224b5745e32a9632052f9daa0c66b0f
arpitgarg123 <arpitgarg424@gmail.com>
2024-11-06 16:15:14 +0530

    fix2

--- backend/utils/Cloudinary.js
+++ backend/utils/Cloudinary.js
@@ -3,7 +3,7 @@ const cloudinary = require("cloudinary").v2;
 
 
 cloudinary.config({ 
-    cloud_name: process.env.cloud_name,
+    cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY, 
     api_secret: process.env.API_KEY_SECRET
 });
